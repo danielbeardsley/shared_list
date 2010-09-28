f608a75d6c96aa5efbc50c6e91ccef6bddaef875
Daniel Beardsley <daniel.beardsley@gmail.com>
2010-09-28 02:34:51 -0700

    Initial Commit - freshly generated rails 3 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaskList::Application.config.secret_token = 'aee8b8b1988855b9ce515487d30a53cb5ecfa1fc0a37e210e75332ded9eb039346c88488be8207493f24f13a2dc74c9b6d91cb23e38d9a7b806ff32e1e7cc278'
