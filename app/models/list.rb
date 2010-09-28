e5e6938c9820976c790164fe5b86bfbc464be7ae
Daniel Beardsley <daniel.beardsley@gmail.com>
2010-09-28 04:51:15 -0700

    Added the bare bones List model

---
+++ app/models/list.rb
@@ -0,0 +1,3 @@
+class List < ActiveRecord::Base
+	attr_readonly :created_at
+end
---
+++ test/unit/list_test.rb
@@ -0,0 +1,5 @@
+require 'test_helper'
+
+class ListTest < ActiveSupport::TestCase
+	should have_readonly_attribute :created_at
+end
