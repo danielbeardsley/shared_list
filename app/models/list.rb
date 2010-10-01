2df6a4538796916094d0ce2e860ab373d1966fae
Daniel Beardsley <daniel.beardsley@gmail.com>
2010-10-01 01:10:27 -0700

    Made List.url unique

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,4 @@
 class List < ActiveRecord::Base
 	attr_readonly :created_at
+	validates_uniqueness_of :url
 end
--- test/unit/list_test.rb
+++ test/unit/list_test.rb
@@ -2,4 +2,11 @@ require 'test_helper'
 
 class ListTest < ActiveSupport::TestCase
 	should have_readonly_attribute :created_at
+	
+	context "a List" do
+		setup do
+			Factory.create :list
+		end
+		should validate_uniqueness_of :url
+	end
 end
