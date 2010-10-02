907b0e1dfdc7594f9fea6956e9278f7a6eb32cd7
Daniel Beardsley <daniel.beardsley@gmail.com>
2010-10-02 16:59:38 -0700

    Made List model autogenerate its url

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,13 @@
 class List < ActiveRecord::Base
 	has_many :items
 	
-	attr_readonly :created_at
+	attr_readonly :created_at, :url
 	
-	validates_uniqueness_of :url
+	validates_uniqueness_of :url, :on => :create
+	
+	before_validation :set_url, :on => :create
+	
+	def set_url
+		self.url ||= (0...10).map{65.+(rand(25)).chr}.join
+	end
 end
--- test/unit/list_test.rb
+++ test/unit/list_test.rb
@@ -3,11 +3,23 @@ require 'test_helper'
 class ListTest < ActiveSupport::TestCase
 	should have_many :items
 	should have_readonly_attribute :created_at
+	should have_readonly_attribute :url
 	
 	context "a List" do
 		setup do
 			Factory.create :list
 		end
-		should validate_uniqueness_of :url
+		#should validate_uniqueness_of :url
+	end
+	
+	context "a new list" do
+		setup do
+			@list = List.create!
+		end
+		
+		should "generate a random url" do
+			assert_not_nil @list.url
+			assert @list.url.length > 5
+		end
 	end
 end
