ad02db5bc17e31c898cb9a8e1a005afb02d10f20
Daniel Beardsley <daniel.beardsley@gmail.com>
2010-10-17 01:16:19 -0700

    javascript: Added ListUI and ItemUI classes

--- public/javascripts/items.js
+++ public/javascripts/items.js
@@ -1,12 +1,18 @@
 function Item(list){
 	this.list = list;
 	DefineEvents(this, 'item_changed','item_deleted');
+	
+	this.set('title', 'New Item ' + Math.random());
 }
 
 
 $.extend(Item.prototype, {
-	load: function(){
-		
-	}
 });
 
+
+function ItemUI(opts){
+	this.item = opts.item;
+	this.element = $('<input type="text"/>');
+	this.element.val(this.item.title);
+	opts.container.append(this.element);
+}
--- public/javascripts/lists.js
+++ public/javascripts/lists.js
@@ -4,9 +4,9 @@ $("#content").keypress(function(e) {
 		}
 });
 
-function List(element){
-	this.element = element
+function List(){
 	DefineEvents(this, 'item_created','list_created','item_deleted');
+	this.items = [];
 }
 
 $.extend(List.prototype, {
@@ -15,9 +15,29 @@ $.extend(List.prototype, {
 	},
 	
 	create_item: function(){
-		
+		var new_item = new Item(this);
+		this.items.push(new_item);
+		this.item_created.fire(new_item);
 	}
 });
+
+
+
+function ListUI(opts){
+	var me       = this,
+	    item_uis = [];
+
+	this.list = opts.list;
+	this.container = opts.container;
+	this.element = $('<div><div class="title">' + this.list.title + '</div></div>');
+	this.container.append(this.element);
+	
+	this.list.item_created.observe(function(new_item){
+		console.log('item created!! ' + new Date());
+		var item_ui = new ItemUI({item:new_item, container:me.element})
+		item_uis.push(item_ui);
+	});
+}
 
 
 
