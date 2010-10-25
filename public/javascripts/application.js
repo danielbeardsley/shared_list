3d1c2499bd7669f177c116f4699a39b6a8ef03bf
Daniel Beardsley <daniel.beardsley@gmail.com>
2010-10-24 17:59:55 -0700

    Moved some code from the show view into an App js object

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,16 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+var App = new (function(){
+	this.create_list = function(attr){
+		var list = new List();
+		if(attr)
+			list.load(attr);
+			
+		current_list_ui = new ListUI({
+			list: list,
+			container:$('.list_container')
+		});
+
+		ListIO.watch_list(list);
+		
+		App.current_list = list;
+	}
+})();
